1796d4da7a146ff3da7e877755559ef72c9d404a
Fredy Morales <fredymorales@fredys-mbp.mynet>
2022-02-27 21:11:13 +0100

    rep

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+const a = 12;
+
 test('renders learn react link', () => {
-    const a = 2;
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+    const b = 5;
 });
