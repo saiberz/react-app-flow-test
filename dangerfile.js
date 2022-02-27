f2c6f94cb36de7905e08fd9271e280f731911abc
Fredy Morales <fredymorales@fredys-mbp.mynet>
2022-02-27 15:37:29 +0100

    Add error

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,20 @@
 import {message, danger} from "danger"
+import { ESLint } from "eslint"
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changes in this PR: \n - " + modifiedMD) 
+(async function main() {
+
+    const mof = danger.git.modified_files
+    // 1. Create an instance.
+    const eslint = new ESLint();
+
+    // 2. Lint files.
+    const results = await eslint.lintFiles(mof);
+
+    // 3. Format the results.
+    const formatter = await eslint.loadFormatter("stylish");
+    const resultText = formatter.format(results);
+
+    // 4. Output it.
+    message(resultText);
+})()
+
