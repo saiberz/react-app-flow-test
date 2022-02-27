2a62ef9851730483721075b841aa431a366cbfbe
Fredy Morales <fredymorales@fredys-mbp.mynet>
2022-02-27 17:57:23 +0100

    Use token

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,7 @@
-import {message, danger} from "danger"
+const {message, danger} = require("danger")
 const { ESLint } = require("eslint");
+const github = require('@actions/github');
+const core = require('@actions/core');
 
 
 const lint = async (files) => {
@@ -14,7 +16,15 @@ const lint = async (files) => {
 
 (async function main() {
 
+    const token = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(token)
+
+    console.log({git: danger.git})
+    console.log({github: danger.github})
+
     const mof = danger.git.modified_files
+
+    console.log({mof})
 
     lint(mof)
 
